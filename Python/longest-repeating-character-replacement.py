599650ff397223b4e391929f7c087d750af9080f
Samuel <samuelsun1888@gmail.com>
2016-10-28 23:48:26 -0400

    Update longest-repeating-character-replacement.py

--- Python/longest-repeating-character-replacement.py
+++ Python/longest-repeating-character-replacement.py
@@ -30,6 +30,7 @@
 # Explanation:
 # Replace the one 'A' in the middle with 'B' and form "AABBBBA".
 # The substring "BBBB" has the longest repeating letters, which is 4.
+#23234
 
 class Solution(object):
     def characterReplacement(self, s, k):
